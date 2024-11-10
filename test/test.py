959dd5baedd79edfe6a019651da3d5cfc22b139d
Devesh B <65714343+devesh-b@users.noreply.github.com>
2024-11-10 12:16:37 +0530

    Added testbench

--- test/test.py
+++ test/test.py
@@ -1,6 +1,3 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
-# SPDX-License-Identifier: Apache-2.0
-
 import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import ClockCycles
@@ -22,19 +19,16 @@ async def test_project(dut):
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
-
     dut._log.info("Test project behavior")
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+    # Set the input values you want to test //a1
+    dut.ui_in.value = 40
+    dut.uio_in.value = 20
 
     # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
-
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    await ClockCycles(dut.clk, 2)
+    assert dut.uio_out.value == 3
+    assert dut.uo_out.value == 32
 
     # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    # one or more clock cycles, and asserting the expected output values
