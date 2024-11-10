e7dd1401df5e25f35ca3a9355b75cc8d255d3262
Devesh B <65714343+devesh-b@users.noreply.github.com>
2024-11-10 12:17:26 +0530

    Added testbench

--- test/tb.v
+++ test/tb.v
@@ -5,14 +5,12 @@
    that can be driven / tested by the cocotb test.py.
 */
 module tb ();
-
   // Dump the signals to a VCD file. You can view it with gtkwave.
   initial begin
     $dumpfile("tb.vcd");
     $dumpvars(0, tb);
     #1;
   end
-
   // Wire up the inputs and outputs:
   reg clk;
   reg rst_n;
@@ -22,20 +20,13 @@ module tb ();
   wire [7:0] uo_out;
   wire [7:0] uio_out;
   wire [7:0] uio_oe;
-`ifdef GL_TEST
-  wire VPWR = 1'b1;
-  wire VGND = 1'b0;
-`endif
-
   // Replace tt_um_example with your module name:
-  tt_um_example user_project (
-
+  tt_um_db_MAC user_project (
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
-      .VPWR(VPWR),
-      .VGND(VGND),
+      .VPWR(1'b1),
+      .VGND(1'b0),
 `endif
-
       .ui_in  (ui_in),    // Dedicated inputs
       .uo_out (uo_out),   // Dedicated outputs
       .uio_in (uio_in),   // IOs: Input path
@@ -45,5 +36,4 @@ module tb ();
       .clk    (clk),      // clock
       .rst_n  (rst_n)     // not reset
   );
-
 endmodule
